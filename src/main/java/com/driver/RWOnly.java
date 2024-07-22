f845b36c85a736f527769acebae9a9f4615c71da
pratik shelar <pratikshelar1727@gmail.com>
2024-07-22 20:52:42 +0530

    Assignment Submitted

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String Name;
+    public void Stter(String name){
+        this.Name = name;
+    }
+    public String Gtter(){
+        return this.Name;
+    }
 }
